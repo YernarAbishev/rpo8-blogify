9373a202298ea97f44d0dcd7c0c2a75f0fe38666
yereke <yereke@MacBook-Air-yereke.local>
2024-02-13 15:13:39 +0600

    first commit

---
+++ news_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def home_page(request):
+    posts = Post.objects.all()[:4]
+    return render(request, "./primary/index.html", {'posts': posts})
+
+def news_page(request):
+    return render(request, "./primary/news.html")
+
+def news_detail_page(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "./primary/news-detail.html", {'post': post})
